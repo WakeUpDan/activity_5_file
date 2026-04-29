0abaa1efac3c1401430cd1a5f06d44b699b0b112
Dan <dllaneza552@gmail.com>
2026-04-29 18:10:23 +0800

    separates number into even and odd

--- first_program/P1.py
+++ first_program/P1.py
@@ -26,3 +26,22 @@ class NumberSorter:
         self.data = data
         self.evens = []
         self.odds = []
+    
+    def sort(self):
+        for n in self.data:
+            if n % 2 == 0:
+                self.evens.append(n)
+            else:
+                self.odds.append(n)
+
+    def save(self, even_file, odd_file):
+        """Saves the sorted results to respective files."""
+        try:
+            with open(even_file, 'w') as fe:
+                fe.write("\n".join(map(str, self.evens)) + "\n")
+            with open(odd_file, 'w') as fo:
+                fo.write("\n".join(map(str, self.odds)) + "\n")
+            return True
+        except IOError as e:
+            print(f"File writing error: {e}")
+            return False
